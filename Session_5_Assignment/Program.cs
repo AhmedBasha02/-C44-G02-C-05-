9d3b28d1a660beaeac7b5cdcce32e415e2dd61a0
AhmedBasha02 <ahmed.bashaa0220@gmail.com>
2025-07-04 07:29:20 +0300

    Add project files.

---
+++ Session_5_Assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Session_5_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
